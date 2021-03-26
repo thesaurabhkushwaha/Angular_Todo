2a17bb63ac99f020c4ac64b79f62e2d17b8630e0
Saurabh Kushwaha <saurabh.sprk@gmail.com>
2021-03-26 17:31:45 +0530

    Initial commit

---
+++ src/app/components/todos/todos.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodosComponent } from './todos.component';
+
+import {Todo} from '../../models/Todo';
+
+describe('TodosComponent', () => {
+  let component: TodosComponent;
+  let fixture: ComponentFixture<TodosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TodosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
